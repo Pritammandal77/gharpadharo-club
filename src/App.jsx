a845ee662cb38bc58c57d8b8cd9c5d97fa3d9f17
Pritam Mandal <pritampmandal@gmail.com>
2025-08-05 15:25:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='bg-green-200 h-20 w-20 text-4xl w-full'>Hello</p>
+
+    </>
+  )
+}
+
+export default App
