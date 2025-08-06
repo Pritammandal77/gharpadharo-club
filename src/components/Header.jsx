c7d4ceb6d8b986b95914041bb3a9469656a75899
Pritam Mandal <pritampmandal@gmail.com>
2025-08-06 20:46:33 +0530

    done some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,8 @@ import React from 'react';
 function Header() {
     return (
         <header>
-            {/* <nav className='h-18 manrope-font bg-[#ffffff] flex items-center font-semibold' style={{ boxShadow: '0px 10px 10px 1px rgba(0,0,0,0.1)' }}>
-                    <div className='flex items-center justify-center gap-1'>
-                        <img src="/logo.webp" alt="" className='h-15 w-15'/>
-                        <p className='text-[18px]'>GharPadharo Club</p>
-                    </div>
-            </nav>
-             */}
             <nav
-                className="h-18 w-[100vw] px-3 md:px-10 lg:px-15 absolute top-0 manrope-font bg-white/10 backdrop-blur-md flex items-center font-semibold "
+                className="h-18 w-[100vw] px-3 md:px-10 lg:px-15 fixed top-0 manrope-font bg-white/10 backdrop-blur-md flex items-center font-semibold "
             >
                 <div className="flex items-center justify-center gap-1">
                     <img src="/logo.webp" alt="" className="h-12 w-12" />
@@ -24,3 +17,13 @@ function Header() {
 }
 
 export default Header;
+
+
+//old header
+{/* <nav className='h-18 manrope-font bg-[#ffffff] flex items-center font-semibold' style={{ boxShadow: '0px 10px 10px 1px rgba(0,0,0,0.1)' }}>
+                    <div className='flex items-center justify-center gap-1'>
+                        <img src="/logo.webp" alt="" className='h-15 w-15'/>
+                        <p className='text-[18px]'>GharPadharo Club</p>
+                    </div>
+            </nav>
+             */}
