cdebc2c83fad4e2ed570771795b1f0b95a8ce817
Pritam Mandal <pritampmandal@gmail.com>
2025-08-13 10:59:28 +0530

    added animations to the gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -24,7 +24,7 @@ function Gallery() {
             id: '1',
             img: '/gallery/image4.jpg'
         },
-         {
+        {
             id: '2',
             img: '/gallery/image9.png'
         },
@@ -43,7 +43,7 @@ function Gallery() {
             id: '2',
             img: '/gallery/image8.png'
         },
-         {
+        {
             id: '4',
             img: '/gallery/image10.png'
         }
@@ -54,7 +54,7 @@ function Gallery() {
                 <div>
                     <h1 className="text-4xl md:text-5xl font-semibold">Image <span className="text-[#4c50a9]">Gallery</span> </h1>
                 </div>
-                <div className='flex justify-center gap-1 xl:gap-5 w-full px-1 xl:px-0'>
+                {/* <div className='flex justify-center gap-1 xl:gap-5 w-full px-1 xl:px-0 animate-marquee '>
                     <div className='flex flex-col w-[33vw] xl:w-[25vw] gap-1 xl:gap-5'>
                         {
                             imageCol1 && imageCol1.map((data, index) => (
@@ -76,7 +76,67 @@ function Gallery() {
                             ))
                         }
                     </div>
+                </div> */}
+                <div className="overflow-hidden w-full">
+                    <div className="animate-marquee">
+                        {/* First set */}
+                        <div className="flex justify-center gap-2 xl:gap-5 px-1 xl:px-2">
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol1.map((data, index) => (
+                                    <img src={data.img} key={`col1-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol2.map((data, index) => (
+                                    <img src={data.img} key={`col2-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol3.map((data, index) => (
+                                    <img src={data.img} key={`col3-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                        </div>
+
+                        {/* Duplicate set */}
+                        <div className="flex justify-center gap-2 xl:gap-5 px-1 xl:px-2">
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol1.map((data, index) => (
+                                    <img src={data.img} key={`dup1-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol2.map((data, index) => (
+                                    <img src={data.img} key={`dup2-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol3.map((data, index) => (
+                                    <img src={data.img} key={`dup3-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className="flex justify-center gap-1 xl:gap-5 px-1 xl:px-2">
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol1.map((data, index) => (
+                                    <img src={data.img} key={`dup1-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol2.map((data, index) => (
+                                    <img src={data.img} key={`dup2-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                            <div className="flex flex-col w-[33vw] xl:w-[25vw] gap-2 xl:gap-5">
+                                {imageCol3.map((data, index) => (
+                                    <img src={data.img} key={`dup3-${index}`} alt="" className="w-full h-auto xl:rounded-xl" />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </>
     );
